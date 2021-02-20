53a2d69f659ee788aed3a8f53061aa7d367cd39e
geoburd <dereinzigefuer@protonmail.com>
2021-02-20 01:50:23 +0100

    v0.2

--- get_price.py
+++ get_price.py
@@ -1,11 +1,12 @@
 URL = 'https://www.poeprices.info/dashboard'
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
-def get_price(searchterm, headless):
+
+def get_price(search_for, headless):
     webpage = r"https://www.poeprices.info/#pricesingleitem"
 
     options = Options()
@@ -13,30 +14,49 @@ def get_price(searchterm, headless):
     if headless:
         options.add_argument('--headless')
 
-    options.add_argument('window-size=1920x1480')
+    options.add_argument("--window-size=1920,1080")
+    options.add_argument("--disable-extensions")
+    options.add_argument("--proxy-server='direct://'")
+    options.add_argument("--proxy-bypass-list=*")
+    options.add_argument("--start-maximized")
+
+    options.add_argument('--disable-gpu')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--no-sandbox')
     options.add_argument('--ignore-certificate-errors')
-    options.add_argument('--allow-running-insecure-content')
-    driver = webdriver.Chrome(executable_path="chromedriver.exe", options=options)
+    capabilities = {
+        'browserName': 'chrome',
+        'chromeOptions': {
+            'useAutomationExtension': False,
+            'forceDevToolsScreenshot': True,
+            'args': ['--start-maximized', '--disable-infobars']
+        }
+    }
+    driver = webdriver.Chrome(executable_path="geckodriver.exe", options=options, desired_capabilities=capabilities)
 
     driver.get(webpage)
 
     sbox = driver.find_element_by_name('itemtext')
-    sbox.send_keys(searchterm)
+    sbox.send_keys(search_for)
 
     submit = driver.find_element_by_id("submitsingle")
     driver.execute_script("arguments[0].click();", submit)
     driver.switch_to.window(window_name=driver.window_handles[-1])
     try:
-        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '/html/body/div/form/table[2]/tbody/tr/td[2]/div[1]/table/tbody/tr/td[1]/table/tbody/tr[2]/td/p[2]/span')))
-        #print(driver.page_source)
+        WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.XPATH,
+                                                                        '/html/body/div/form/table[2]/tbody/tr/td['
+                                                                        '2]/div[1]/table/tbody/tr/td['
+                                                                        '1]/table/tbody/tr[2]/td/p[2]/span')))
+        # print(driver.page_source)
 
-        price = str(driver.find_element_by_xpath('/html/body/div/form/table[2]/tbody/tr/td[2]/div[1]/table/tbody/tr/td[1]/table/tbody/tr[2]/td/p[2]/span').text)
+        price = str(driver.find_element_by_xpath(
+            '/html/body/div/form/table[2]/tbody/tr/td[2]/div[1]/table/tbody/tr/td[1]/table/tbody/tr[2]/td/p[2]/span').text)
         if headless:
-            driver.close()
+            driver.quit()
 
         return price
     except:
         with open('page.html', 'w') as f:
             f.write(driver.page_source)
-        driver.close()
-        return '999 Chaos'
+        driver.quit()
+        return '9999 Chaos'
--- screen.py
+++ screen.py
@@ -10,18 +10,21 @@ import numpy as np
 
 pyautogui.FAILSAFE = False
 
+
 def make_screenshot(rect):
     with mss.mss() as mss_instance:
-
         screenshot = mss_instance.grab(rect)
 
         img = Image.frombytes("RGB", screenshot.size, screenshot.bgra, "raw", "BGRX")  # Convert to PIL.Image
         img.save('screenshot' + '.png', 'PNG')
 
+
 stash = {"left": 17, "top": 150, "width": 580, "height": 580}
 prices = []
+
+
 def find_bright(img):
-    pyautogui.moveTo(20,20)
+    pyautogui.moveTo(20, 20)
     pyautogui.click()
     time.sleep(0.5)
     pyautogui.click()
@@ -39,41 +42,66 @@ def find_bright(img):
                             cv2.CHAIN_APPROX_SIMPLE)
     cnts = imutils.grab_contours(cnts)
     for (i, c) in enumerate(cnts):
+        text = 'error in copypaste'
         ((x, y), _) = cv2.minEnclosingCircle(c)
         cv2.putText(image, "#{}".format(i + 1), (int(x) - 45, int(y) + 20),
                     cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 3)
         cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
-        pyautogui.moveTo(int(x) +17, int(y) +150)
+        pyautogui.moveTo(int(x) + 17, int(y) + 150)
 
         pyautogui.hotkey('ctrl', 'c')
         text = clipboard.paste()
         print(text)
         if "price" not in text:
             price = get_price(str.join("\n", text.splitlines()), True)
-            price=price[:-7]
-            print(price)
-            cv2.imwrite('out.png',  image)
-            prices.append(price)
+            if 'Chaos' in price:
+                price = price[:-6]
+                print(price)
+                cv2.imwrite('out.png', image)
+                prices.append(price)
 
-            pyautogui.rightClick(int(x) + 17, int(y) + 150, duration=0.1)
+                pyautogui.rightClick(int(x) + 17, int(y) + 150, duration=0.1)
 
-            pyautogui.moveTo(pyautogui.locateCenterOnScreen('select.png', confidence=0.5), duration=0.1)
-            pyautogui.click(duration=0.1)
+                pyautogui.moveTo(pyautogui.locateCenterOnScreen('select.png', confidence=0.5), duration=0.1)
+                pyautogui.click(duration=0.1)
 
-            pyautogui.moveTo(pyautogui.locateCenterOnScreen('exact.png', confidence=0.7), duration=0.1)
-            pyautogui.click(duration=0.1)
+                pyautogui.moveTo(pyautogui.locateCenterOnScreen('exact.png', confidence=0.7), duration=0.1)
+                pyautogui.click(duration=0.1)
 
-            pyautogui.moveTo(pyautogui.locateCenterOnScreen('field_for_price.png', confidence=0.7), duration=0.1)
-            pyautogui.click(duration=0.1)
+                pyautogui.moveTo(pyautogui.locateCenterOnScreen('field_for_price.png', confidence=0.7), duration=0.1)
+                pyautogui.click(duration=0.1)
 
-            pyautogui.write(price, interval=0.25)
-            ok=pyautogui.locateCenterOnScreen('ok.png', confidence=0.7)
-            pyautogui.moveTo(ok, duration=0.1)
-            pyautogui.click(duration=0.1)
+                pyautogui.write(price, interval=0.25)
+                ok = pyautogui.locateCenterOnScreen('ok.png', confidence=0.7)
+                pyautogui.moveTo(ok, duration=0.1)
+                pyautogui.click(duration=0.1)
 
+            if 'Exalted' in price:
+                price = price[:-8]
+                print(price)
+                cv2.imwrite('out.png', image)
+                prices.append(price)
+
+                pyautogui.rightClick(int(x) + 17, int(y) + 150, duration=0.1)
+
+                pyautogui.moveTo(pyautogui.locateCenterOnScreen('select.png', confidence=0.5), duration=0.1)
+                pyautogui.click(duration=0.1)
+
+                pyautogui.moveTo(pyautogui.locateCenterOnScreen('exact.png', confidence=0.7), duration=0.1)
+                pyautogui.click(duration=0.1)
+
+                pyautogui.moveTo(pyautogui.locateCenterOnScreen('select_unit.png', confidence=0.7), duration=0.1)
+                pyautogui.click(duration=0.1)
+
+                pyautogui.moveTo(pyautogui.locateCenterOnScreen('exalted.png', confidence=0.7), duration=0.1)
+                pyautogui.click(duration=0.1)
+
+                pyautogui.moveTo(pyautogui.locateCenterOnScreen('field_for_price.png', confidence=0.7), duration=0.1)
+                pyautogui.click(duration=0.1)
+
+                pyautogui.write(price, interval=0.25)
+                ok = pyautogui.locateCenterOnScreen('ok.png', confidence=0.7)
+                pyautogui.moveTo(ok, duration=0.1)
+                pyautogui.click(duration=0.1)
 
     return prices
-
-#time.sleep(3)
-#make_screenshot(stash)
-#find_bright('screenshot.png')
